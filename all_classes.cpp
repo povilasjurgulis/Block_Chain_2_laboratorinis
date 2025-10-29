7bdf4a80373f7b2ddf351aed2b5cba6d20278fc9
Povilas Jurgulis <povilas.jurgulis@mif.stud.vu.lt>
2025-10-29 13:57:53 +0200

    Pridėta usergeneration implementacija

--- all_classes.cpp
+++ all_classes.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 using namespace std::chrono;
 
-// ---------------------- Transaction (implementation) ----------------------
+// ---------------------- Transaction ----------------------
 Transaction::Transaction() : amount(0) {
     timestamp = duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
     generateTransactionId();
@@ -42,7 +42,7 @@ void Transaction::displayTransaction() const {
                "Amount: " + std::to_string(amount) + "\n");
 }
 
-// ---------------------- User (implementation) ----------------------
+// ---------------------- User ----------------------
 User::User() : balance(0.0) { generatePublicKey(); }
 
 User::User(const string& name, double initial_balance)
@@ -68,4 +68,49 @@ void User::generatePublicKey() {
 void User::displayUser() const {
     print_both("User: " + name + " | Public Key: " + public_key.substr(0, 16) +
                "... | Balance: " + std::to_string(balance) + "\n");
+}
+
+// ---------------------- UserGenerator ----------------------
+mt19937 UserGenerator::rng;
+std::uniform_int_distribution<int> UserGenerator::balance_dist(100, 1000000);
+
+void UserGenerator::initializeRandom() {
+    random_device rd; rng.seed(rd());
+}
+
+vector<string> UserGenerator::generateRandomNames(int count) {
+    vector<string> names; names.reserve(count);
+    for (int i = 1; i <= count; i++) names.push_back("User" + std::to_string(i));
+    return names;
+}
+
+vector<User> UserGenerator::generateUsers(int user_count) {
+    initializeRandom();
+    vector<User> users; users.reserve(user_count);
+    auto names = generateRandomNames(user_count);
+    for (int i = 0; i < user_count; i++) {
+        double balance = balance_dist(rng);
+        User user(names[i], balance);
+        users.push_back(user);
+    }
+    return users;
+}
+
+void UserGenerator::displayUserStats(const vector<User>& users) {
+    if (users.empty()) return;
+    double total_balance = 0.0;
+    double min_balance = users[0].getBalance();
+    double max_balance = users[0].getBalance();
+    for (const auto& user : users) {
+        double balance = user.getBalance();
+        total_balance += balance;
+        min_balance = std::min(min_balance, balance);
+        max_balance = std::max(max_balance, balance);
+    }
+    print_both("User Statistics:\n");
+    print_both("  Total Users: " + std::to_string(users.size()) + "\n");
+    print_both("  Total Balance: " + std::to_string((long long)total_balance) + "\n");
+    print_both("  Average Balance: " + std::to_string((long long)(total_balance / users.size())) + "\n");
+    print_both("  Balance Range: " + std::to_string((long long)min_balance) + " - " +
+               std::to_string((long long)max_balance) + "\n");
 }
